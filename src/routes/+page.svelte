ab67225be7d36b54800ff029729af6bd7bf1bb98
wakeupxavey <xavey.bzdek@berkeley.edu>
2026-04-03 17:08:29 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@ Hard-code custom code that should appear BEFORE Google Doc here.
 This code is in +page.svelte and can be manually coded using HTML or Svelte tags.
 -->
 <header class="container-fluid d-flex flex-column align-items-center justify-content-center vh-100 mb-4" style="background-image:url('image/zshit1.png');background-size:cover;background-position:center;">
-  <div class="container text-center col-12 align-items-top justify-content-center">
+  <div class="container text-center col-12 col-sm-10 col-lg-8 col-xxl-6  justify-content-top">
           <h1 class="text-white text-center display-1">Multimedia Template 2026</h1>
 
      </div>
